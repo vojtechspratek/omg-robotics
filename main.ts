73a1193a40d24a1d3a1db22b5aa248ebad49b7a9
vojtechspratek <96978714+vojtechspratek@users.noreply.github.com>
2023-05-30 15:03:05 +0200

    Aktualizovat pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,23 +1,44 @@
-let peeled = false
-enum TropicalFruit {
-    Banana = 0,
-    Pinapple = 1,
-    Coconut = 2
-}
-namespace tropic {
+namespace customExtension {
     /**
-     * Pick a fruit
+     * Observe button press with specified input pin.
+     * @param pin The input pin to observe.
+     * @param returnBoolean Whether to return a boolean value indicating the button press.
+     *                      If false, it will generate an event instead.
+     * @returns True if the button is pressed (if returnBoolean is true).
      */
-    export function pick(fruit: TropicalFruit): boolean {
-        return true;
+    // Variant with returning a boolean value
+    // Example usage: let buttonPressed = customExtension.observeButtonPress(pin, true);
+    export function observeButtonPress(pin: number, returnBoolean: boolean): boolean {
+        let buttonPressed = false;
+        if (pins.digitalReadPin(pin) == 0) {
+            buttonPressed = true;
+        }
+
+        if (returnBoolean) {
+            return buttonPressed;
+        } else {
+            control.raiseEvent(
+                EventBusSource.MICROBIT_ID_IO_P0 + pin,
+                EventBusValue.MICROBIT_BUTTON_EVT_CLICK
+            );
+            return false;
+        }
     }
+
     /**
-     * Peel the fruit if possible
+     * Observe button press with specified input pin.
+     * Generates an event when the button is pressed.
+     * @param pin The input pin to observe.
      */
-    export function peel(fruit: TropicalFruit): boolean {
-        return (fruit == TropicalFruit.Banana);
+    // Variant with generating an event
+    // Example usage: customExtension.observeButtonPress(pin, false);
+    export function ButtonPressEvent(pin: number): void {
+        if (pins.digitalReadPin(pin) == 0) {
+            control.raiseEvent(
+                EventBusSource.MICROBIT_ID_IO_P0 + pin,
+                EventBusValue.MICROBIT_BUTTON_EVT_CLICK
+            );
+        }
     }
 }
-if (tropic.pick(TropicalFruit.Banana)) {
-    peeled = tropic.peel(TropicalFruit.Banana);
-}
+
