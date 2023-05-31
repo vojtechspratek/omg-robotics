116d1458ae8eb51d97b76c373d241f20a1a8fcaf
vojtechspratek <96978714+vojtechspratek@users.noreply.github.com>
2023-05-31 10:44:39 +0200

    Aktualizovat main.ts

--- main.ts
+++ main.ts
@@ -3,10 +3,18 @@ namespace omgRobotics {
      * LED control for P0 & P1
      * @param pin The output pin to control the LED.
      */
+
+enum MyEnum{
+    //% block="ON"
+    ON,
+    //% block="OFF"
+    OFF,
+}
+
     //% blockId=omgroboticssetled
-    //% block="(LED) $index"
+    //% block="(LED) $n $e "
     //% index.min=0 index.max=25
-    export function setLed(pin: number): void {
+    export function setLed(pin: number, e: MyEnum): void {
         if (input.onPinPressed(TouchPin.P0, function () {
         })){
             led.toggleAll()
