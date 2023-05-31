e4d685883d2edcce3cbd6b3a212fd3e121545c7c
vojtechspratek <96978714+vojtechspratek@users.noreply.github.com>
2023-05-31 11:05:17 +0200

    Aktualizovat main.ts

--- main.ts
+++ main.ts
@@ -11,10 +11,16 @@ namespace omgRobotics {
         OFF,
     }
 
+    enum MyEnum2 {
+        //% block="P0"
+        P0,
+        //% block="P1"
+        P1,
+    }
+
     //% blockId=omgroboticssetled
     //% block="(LED) $pin $e "
-    //% index.min=0 index.max=25
-    export function setLed(pin: number, e: MyEnum): void {
+    export function setLed(pin: MyEnum2, e: MyEnum): void {
         if (input.onPinPressed(TouchPin.P0, function () {
         })) {
             led.toggleAll()
